5863e1980e852a6f0ec89d732350fa29312bd002
JPedroCh <joaopedroaschaves@gmail.com>
2022-02-04 07:31:13 -0300

    Adicionada funçao de criar grafo bipartido.
    Signed-off-by: JPedroCh <joaopedroaschaves@gmail.com>

---
+++ src/Graph.js
@@ -0,0 +1,46 @@
+class Node {
+    constructor(name, adjacents, nodeType, link, imgRef) {
+        this.name = name;
+        this.adjacents = adjacents;
+        this.nodeType = nodeType; 
+        this.link = link;
+        this.imgRef = imgRef;
+    };
+};
+
+const receitas = [
+    ["Bolovo", ["Sal", "Ovo", "Carne Moida", "Leite", "Pimenta-do-Reino",
+    "Farinha de Rosca","Óleo"],
+    "https://www.tudogostoso.com.br/receita/199322-bolovo.html",
+    "https://feedmechannel.com/wp-content/uploads/2019/06/receita-de-bolovo-1024x683.jpg"],
+
+    ["Bolinho de Bacalhau", ["Sal", "Óleo", "Farinha de Trigo", "Cheiro-Verde",
+    "Batata", "Bacalhau","Ovo"],
+    "https://www.tudogostoso.com.br/receita/1979-bolinho-de-bacalhau.html",
+    "https://t2.rg.ltmcdn.com/pt/posts/7/2/1/bolinho_de_bacalhau_127_600_square.jpg"],
+
+    ["Amendoim Temperado", ["Sal", "Amendoim", "Azeite", "Páprica Doce", "Páprica Defumada"],
+    "https://www.tudogostoso.com.br/receita/200325-amendoim-temperado.html",
+    "https://mesaesabor.com.br/wp-content/uploads/2020/09/36.jpg"]
+];
+
+const ingredientes = [
+    "Ovo", "Carne Moida", "Leite", "Pimenta-do-Reino", "Farinha de Rosca", 
+    "Óleo", "Farinha de Trigo", "Cheiro-Verde", "Batata", "Bacalhau",
+    "Amendoim", "Azeite", "Páprica Doce", "Páprica Defumada"
+];
+
+export default function createGraph() {
+    let graph = [];
+
+    for(let i = 0; i < receitas.length; i++){
+        let recipe = new Node(receitas[i][0], receitas[i][1], "receita", receitas[i][2], receitas[i][3]);
+        graph.push(recipe);
+    }
+
+    for(let i = 0; i < ingredientes.length; i++){
+        let ingredient = new Node(ingredientes[i],[], "ingrediente", "no link", "no image");
+        graph.push(ingredient);
+    }
+    return graph;
+}
--- src/Homepage.js
+++ src/Homepage.js
@@ -4,12 +4,17 @@ import {
   ButtonDiv
 } from './Style';
 import IngredientCard from './Components/IngredientCard';
+import createGraph from './Graph';
 
 const Homepage = () => {
 
   const ingredients = ["Ovo", "CarneMoida", "Leite", "PimentaDoReino", "FarinhaDeRosca", 
   "Oleo", "FarinhaDeTrigo", "CheiroVerde", "Batata", "Bacalhau",
   "Amendoim", "Azeite", "PapricaDoce", "PapricaDefumada"];
+
+  const graph = createGraph();
+
+  console.log(graph);
 
   return (
     <BackgroundContainer>    
